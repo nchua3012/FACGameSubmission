f51c12b2d5268ac0074b4a8f5bd59c2925708536
nchua3012 <n.chua3012@googlemail.com>
2024-10-28 11:15:46 +0000

    Basic movement for player and enemy added

---
+++ index.js
@@ -0,0 +1,176 @@
+const canvas = document.querySelector ('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.3
+
+class Sprite {
+constructor({position, velocity}) {
+    this.position = position
+    this.velocity = velocity
+    this.height = 150
+    this.lastKey = null
+    }
+
+    draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+    this.draw()
+
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+        this.position.y = canvas.height - this.height; 
+    } else this.velocity.y += gravity
+    }
+}
+
+// Player 
+
+const player = new Sprite({
+    position: {
+        x:0,
+        y:0
+    },
+    velocity: {
+        x:0,
+        y:0
+    },
+})
+
+player.draw()
+
+// Enemy
+
+const enemy = new Sprite({
+    position: {
+        x:400,
+        y:100
+    },
+    velocity: {
+        x:0,
+        y:0
+    },
+})
+    
+enemy.draw()
+
+// Player Movement
+
+const keys = {
+    a: {
+        pressed:false
+    },
+    d: {
+        pressed:false
+    },
+    w: {
+        pressed:false
+    },
+
+// Enemy Movement
+
+    ArrowRight: {
+        pressed:false
+    },
+    ArrowLeft: {
+        pressed:false
+    },
+    ArrowUp: {
+        pressed:false
+    },
+
+}
+let lastKey
+
+
+function animate () {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas. height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x = 0
+    enemy.velocity.x = 0
+
+// Player Movement
+    
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -3
+    } else if (keys.d.pressed&& player.lastKey === 'd'){
+        player.velocity.x = 3
+    }
+// Enemy Movement
+    
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -3
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
+        enemy.velocity.x = 3
+    }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+
+// Player Movement 
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break  
+        case 'w':
+            player.velocity.y = -10
+            break
+
+
+// Enemy Movement        
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -10
+            break
+
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+
+// Player Movement    
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break   
+
+// Enemy Movement
+        case 'ArrowRight':
+                keys.ArrowRight.pressed = false
+                break
+        case 'ArrowLeft':
+                keys.ArrowLeft.pressed = false
+                break  
+        
+    }
+})

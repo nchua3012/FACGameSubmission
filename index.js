01e435686f7fa6092438b31f6a7df65d869247b2
nchua3012 <n.chua3012@googlemail.com>
2024-11-05 11:01:13 +0000

    Added timer and determining game winner

--- index.js
+++ index.js
@@ -144,6 +144,37 @@ function rectangularCollision ({rectangle1, rectangle2}) {
     )
 }
 
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector ('#gameEndTie').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector ('#gameEndTie').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector ('#gameEndTie').innerHTML = 'Player 1 Wins'
+    } else if (player.health < enemy.health)
+        document.querySelector ('#gameEndTie').innerHTML = 'Player 2 Wins'    
+    }
+
+
+let timer = 30
+let timerId
+function decreaseTimer() {
+    
+    if (timer > 0) {
+        timerId = setTimeout (decreaseTimer, 1000)
+        timer--
+    document.querySelector ('#timer').innerHTML = timer
+    }
+
+    if (timer === 0) {
+        document.querySelector ('#gameEndTie').style.display = 'flex'
+        determineWinner({player, enemy, timerId})
+    }
+
+}
+
+decreaseTimer()
+
 function animate () {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -197,7 +228,18 @@ function animate () {
         player.health -= 10
         document.querySelector ('#playerHealth').style.width = player.health + "%"
     }
+
+//End game based on health
+
+if (enemy.health <=0 || player.health <=0 ) {
+    determineWinner ({player, enemy, timerId})
 }
+
+
+}
+
+
+
 
 animate()
 

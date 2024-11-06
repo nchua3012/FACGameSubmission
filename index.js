8e161febf0bb540cd9a614a93742db228799b68c
nchua3012 <n.chua3012@googlemail.com>
2024-11-06 10:15:31 +0000

    Seperating classes into seperate file for clarity

--- index.js
+++ index.js
@@ -9,67 +9,21 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.3
 
 class Sprite {
-constructor({position, velocity, color = 'red', offset }) {
+constructor({position}) {
     this.position = position
-    this.velocity = velocity
     this.width = 50
     this.height = 150
-    this.lastKey = null
-    this.attackBox = {
-        position: {
-        x: this.position.x,
-        y: this.position.y
-        },
-        offset,
-            width: 100,
-            height: 50,
-        },
-        
-
-        this.color = color
-        this.isAttacking = false
-        this.health = 100
     }
-
 draw() {
-
-    c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-    // attack box
-    if (this.isAttacking) {
-        c.fillStyle = 'green'
-        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
 }
-        
     update() {
     this.draw()
-
-    this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-    this.attackBox.position.y = this.position.y
-
-
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-        this.position.y = canvas.height - this.height; 
-    } else this.velocity.y += gravity
-    }
-
-
-    attack() {
-    this.isAttacking = true
-    setTimeout(() => {
-        this.isAttacking = false
-    }, 100)
     }
 }
     
 // Player 
 
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x:0,
         y:0
@@ -88,7 +42,7 @@ player.draw()
 
 // Enemy
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x:400,
         y:100
